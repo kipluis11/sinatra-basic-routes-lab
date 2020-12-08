fd1d9ea0b26f2653dc9bdf50d09eac028188c932
kip_parcell <kipluis@gmail.com>
2020-12-08 15:55:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Kip"
+    end
+
+    get '/hometown' do
+        "My hometown is Gilbert"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Simple Things"
+    end
+
 end
